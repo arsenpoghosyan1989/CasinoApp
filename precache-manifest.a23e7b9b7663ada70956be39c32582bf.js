6f6ae8edafa2cde9b3b2381e00c2001d306e5713
Arsen Poghosyan <arsenpoghosyan1989@gmail.com>
2020-07-26 10:11:20 +0400

    Updates

---
+++ precache-manifest.a23e7b9b7663ada70956be39c32582bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e00309b5c2c6688a75d0305955513a",
+    "url": "/CasinoApp/index.html"
+  },
+  {
+    "revision": "8940f0fb63bfb08936cf",
+    "url": "/CasinoApp/static/css/main.f4668edb.chunk.css"
+  },
+  {
+    "revision": "366cc7ecaf0516f10d4a",
+    "url": "/CasinoApp/static/js/2.38ed0e35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CasinoApp/static/js/2.38ed0e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8940f0fb63bfb08936cf",
+    "url": "/CasinoApp/static/js/main.cada517d.chunk.js"
+  },
+  {
+    "revision": "774d5ad8bc28954f3d9c",
+    "url": "/CasinoApp/static/js/runtime-main.fce21c98.js"
+  },
+  {
+    "revision": "9808255fd83ecf77abfa0993dc8b5d07",
+    "url": "/CasinoApp/static/media/1.9808255f.PNG"
+  },
+  {
+    "revision": "0434c4f6e19def8ba2ae37d799f60de0",
+    "url": "/CasinoApp/static/media/2.0434c4f6.PNG"
+  },
+  {
+    "revision": "2fb49cbda86f76be1a8d007dc8d3b663",
+    "url": "/CasinoApp/static/media/3.2fb49cbd.PNG"
+  },
+  {
+    "revision": "8c904e68ab493f195368c6a0050ac56c",
+    "url": "/CasinoApp/static/media/4.8c904e68.PNG"
+  },
+  {
+    "revision": "5d72f711a880415ba0550b21db8cfb89",
+    "url": "/CasinoApp/static/media/5.5d72f711.PNG"
+  },
+  {
+    "revision": "10e88f999a8853215584aabb3b295463",
+    "url": "/CasinoApp/static/media/6.10e88f99.PNG"
+  },
+  {
+    "revision": "d16f4f66226a1fbe14dde1795c5d7b59",
+    "url": "/CasinoApp/static/media/7.d16f4f66.PNG"
+  },
+  {
+    "revision": "cd2b80659a9c4d8bb2962b63bfc2b6aa",
+    "url": "/CasinoApp/static/media/dot.cd2b8065.gif"
+  },
+  {
+    "revision": "bc4423131e95545fcb263767166958aa",
+    "url": "/CasinoApp/static/media/spinner.bc442313.gif"
+  }
+]);
